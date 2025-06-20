c03f8930b29c8b4b0fccaaf12946369538036f27
usama uddin <usamahafeez948@gmail.com>
2025-06-20 16:24:03 +0500

    feat: another push

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Testing CI/CD pipeline with NestJS!';
+    return 'Hello World!!';
   }
 }
